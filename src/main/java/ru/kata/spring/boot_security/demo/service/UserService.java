bb2a41a569306f2d64312c19326574f84f67aa21
Bolotov Alexander <distalker1@gmail.com>
2022-01-19 14:42:52 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserService.java
@@ -0,0 +1,19 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User findById(Long id);
+
+    List<User> findAll();
+
+    void saveUser(User user);
+
+    void deleteById(Long id);
+
+    List<Role> listRoles();
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package ru.kata.spring.boot_security.demo.service;
+
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import java.util.List;
+
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private RoleRepository roleRepository;
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository,
+                           PasswordEncoder passwordEncoder,
+                           RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }
+
+    public User findById(Long id) {
+        return userRepository.getById(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public void saveUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    public void deleteById(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    public List<Role> listRoles() {
+        return roleRepository.findAll();
+    }
+}
